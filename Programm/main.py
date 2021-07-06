197d406ee47533476a228e8246f0b52ea882bc9d
Shad6owh4rd <85156719+Shad6owh4rd@users.noreply.github.com>
2021-07-06 15:40:14 +0200

    Änderungen und speichern der Daten in eine Textdatei

--- Programm/main.py
+++ Programm/main.py
@@ -1,3 +1,4 @@
+from datetime import date
 import random
 
 
@@ -5,17 +6,13 @@ import random
 # Das Ergebnis der Berechnung wird zurückgegeben.
 
 def calculate(a, op, b):
-    if op == 0:
-        print(f'{num1} + {num2} = ')
+    if op == '+':
         return a + b
-    elif op == 1:
-        print(f'{num1} - {num2} = ')
+    elif op == '-':
         return a - b
-    elif op == 2:
-        print(f'{num1} * {num2} = ')
+    elif op == '*':
         return a * b
-    elif op == 3:
-        print(f'{num1} / {num2} = ')
+    elif op == '/':
         if b == 0:
             return None
         return a / b
@@ -38,38 +35,71 @@ def player_entry():
     return x
 
 
+# Aufgaben in einer Textdatei speichern
+
+def safe_exercises(exercises):
+    file = open('files/Aufgaben.txt', 'w')
+    for i in exercises:
+        file.write(str(list(i)))
+        file.write('\n')
+    file = open('files/Aufgaben.txt', 'r')
+    print(file.read())
+    file.close()
+
+
 # Programmstart "Main"
 # Zufallszahlen werden generiert
 # Spieler wird aufgefordert das Ergebnis einzutragen und seinen Namen zu nennen
 # Ergebnis wird berechnet und Anzahl richtiger ausgegeben
+# Problemlösung: random.choice(operator) immer selbes Ergebnis
+#   source: https://stackoverflow.com/questions/10181932/random-choice-always-same
 
 num1 = 0
-operator = 0
+operator = ['+', '-', '*', '/']
+ranOperator = random.Random(500)
 num2 = 0
 result = 0
 entry = 0
 cntCorrect = 0
 cntExercises = 0
 
+# Variablen für die Datenspeicherung auf der Text.txt
+exerciseSheet = 1
+day = date.today()
+correct = False
+correctAnswersAverage = 0
+
+exercises = list()
+
 print('Programm Start\n')
 
 name = input('Bitte gib deinen Namen ein: \n')
 
-for i in range(0, 10):
+for i in range(0, 3):
     num1 = random.randint(0, 10)
-    operator = random.randint(0, 3)
+    ranOperator = random.choice(operator)
     num2 = random.randint(0, 10)
     try:
-        result = float(calculate(num1, operator, num2))
+        result = float(calculate(num1, ranOperator, num2))
     except TypeError:
         result = None
+    print(f'{num1} {ranOperator} {num2} = ')
     entry = player_entry()
     if result != entry:
         print(f'Falsches Ergebnis, richtig wäre: {result}')
+        correct = False
     else:
         cntCorrect += 1
+        correct = True
     cntExercises += 1
+    ex = [name, exerciseSheet, cntExercises, day, num1, ranOperator, num2, entry, result, correct]
+    exercises.append(ex)
 
-print(f'{name} hat {cntCorrect} von {cntExercises} richtig beantwortet!')
+print(f'{name} hat {cntCorrect} von {cntExercises} richtig beantwortet!\n')
+correctAnswersAverage = int(cntCorrect / cntExercises * 100)
+ex = [name, exerciseSheet, cntExercises, cntCorrect, correctAnswersAverage]
+exercises.append(ex)
+
+safe_exercises(exercises)
 
 print('\nProgrammende')
