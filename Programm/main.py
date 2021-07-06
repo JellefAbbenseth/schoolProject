ffe7caef8b866397a04eca2276fac73daf67203f
Shad6owh4rd <85156719+Shad6owh4rd@users.noreply.github.com>
2021-07-06 14:05:26 +0200

    Kommentiert TypeError abgefangen

--- Programm/main.py
+++ Programm/main.py
@@ -1,15 +1,8 @@
 import random
 
-num1 = 0
-operator = 0
-num2 = 0
-result = 0
-entry = 0
-cntCorrect = 0
-cntExercises = 0
 
-print('Programm Start\n')
-
+# Berechnung des Ergebnisses und Mitteilung an den Spieler
+# Das Ergebnis der Berechnung wird zurückgegeben.
 
 def calculate(a, op, b):
     if op == 0:
@@ -28,6 +21,9 @@ def calculate(a, op, b):
         return a / b
 
 
+# Spieleingabe wird ermöglicht
+# Prüfung auf mögliche Eingabefehler mit Neuversuch
+
 def player_entry():
     again = True
     x = 0
@@ -42,11 +38,31 @@ def player_entry():
     return x
 
 
+# Programmstart "Main"
+# Zufallszahlen werden generiert
+# Spieler wird aufgefordert das Ergebnis einzutragen und seinen Namen zu nennen
+# Ergebnis wird berechnet und Anzahl richtiger ausgegeben
+
+num1 = 0
+operator = 0
+num2 = 0
+result = 0
+entry = 0
+cntCorrect = 0
+cntExercises = 0
+
+print('Programm Start\n')
+
+name = input('Bitte gib deinen Namen ein: \n')
+
 for i in range(0, 10):
     num1 = random.randint(0, 10)
     operator = random.randint(0, 3)
     num2 = random.randint(0, 10)
-    result = float(calculate(num1, operator, num2))
+    try:
+        result = float(calculate(num1, operator, num2))
+    except TypeError:
+        result = None
     entry = player_entry()
     if result != entry:
         print(f'Falsches Ergebnis, richtig wäre: {result}')
@@ -54,6 +70,6 @@ for i in range(0, 10):
         cntCorrect += 1
     cntExercises += 1
 
-print(f'Sie haben {cntCorrect} von {cntExercises} richtig beantwortet!')
+print(f'{name} hat {cntCorrect} von {cntExercises} richtig beantwortet!')
 
 print('\nProgrammende')
