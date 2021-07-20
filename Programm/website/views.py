9a7eafc5dc791b632c304c1509d9b0139a6ada62
Shad6owh4rd <85156719+Shad6owh4rd@users.noreply.github.com>
2021-07-20 14:37:43 +0200

    Projektabschnitt 5: - Aufbau der Webseite home - CSS Grundlagen - weitere html seiten (login, registry, exercise_sheet

--- Programm/website/views.py
+++ Programm/website/views.py
@@ -5,8 +5,22 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    text = ''
     if request.method == 'POST':
-        text = request.form.get('input')
+        if request.form.get('login_button'):
+            return render_template('login.html')
+        if request.form.get('exercise_button'):
+            return render_template('exercise_sheet.html')
 
-    return render_template('home.html', output=text)
+    user_name = 'DoJo'
+    first_name = 'John'
+    last_name = 'Doe'
+    exercises = 10
+    average = 90
+    niveau = 3
+    number_exercises = 7
+    average_exercises = 70
+
+    return render_template('home.html', user_name=user_name, first_name=first_name,
+                           last_name=last_name, exercises=exercises, average=average,
+                           niveau=niveau, number_exercises=number_exercises,
+                           average_exercises=average_exercises)
