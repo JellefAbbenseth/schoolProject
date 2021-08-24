60dcc801a81a9e9a4772d2826867d03037f9ff0b
Shad6owh4rd <85156719+Shad6owh4rd@users.noreply.github.com>
2021-08-24 07:52:31 +0200

    Projektabschnitt 5: - Abschluss Projektabschnitt 5 - Grundfunktionen erfüllt

--- Programm/main.py
+++ Programm/main.py
@@ -3,38 +3,6 @@
 from website import create_app
 import webbrowser
 
-
-# Datenbank erstellen
-# Prüfung ob der Benutzer bereits in der Datenbank enthalten ist
-# Sollte er nicht enthalten sein, Abfrage ob er erstellt werden soll
-# Dies geschieht so lange, bis ein "Login" stattgefunden hat
-
-def player_login(database):
-    database.createTables()
-
-    login = False
-    user_name = ''
-    while not login:
-        user_name = input('Bitte gib deinen Benutzernamen ein: \n')
-        name_exists = database.selectUser(user_name)
-
-        if not name_exists:
-            new = input('Willst du einen neuen Benutzer anlegen? Schreibe Ja/Nein\n')
-            if new.lower() == 'ja':
-                first_name = input('Bitte gib deinen Vornamen an: ')
-                last_name = input('Bitte gib deinen Nachnamen an: ')
-                database.newUser(user_name, first_name, last_name)
-                print('Dein Konto wurde angelegt. Viel Spaß')
-                login = True
-            else:
-                print('Bei der Eingabe wurde ein Fehler gemacht, bitte versuche es erneut!')
-                login = False
-        else:
-            login = True
-            print('Erfolgreich angemeldet. Viel Spaß')
-    return user_name
-
-
 # Programmstart "main"
 # Erstellen oder Aufrufen der Datenbank mit Anmeldung
 # Stellen von Aufgaben mit Möglichkeit weitere Aufgaben zu erstellen und bearbeiten
@@ -52,21 +20,5 @@ if __name__ == '__main__':
     webbrowser.open_new_tab(url)
 
     app.run(port=port, debug=False)
-
-# db = Database()
-# name = player_login(db)
-#
-# playing = True
-# while playing:
-#     list_subjects = db.actualNiveau()
-#     dif = Difficulty(db, list_subjects)
-#     dif.chooseExercises()
-#     dif.answerExercises()
-#     choose = input('Möchtest du noch ein Aufgabenblatt lösen? Ja/Nein\n')
-#     if choose.lower() == 'ja':
-#         print('Viel Spaß!')
-#     else:
-#         playing = False
-#         print('Schade, bis zum nächsten Mal.')
 
 print('\nProgrammende')
